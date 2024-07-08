5e15dbb5c2caba5c47b11678a602240d62041ce1
navya7093 <navyakontala@gmail.com>
2024-07-08 22:46:47 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -29,23 +29,23 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_5) {
  //AAA
   char soundex[5];
   generateSoundex("ABFP", soundex);
- // ASSERT_STREQ(soundex,"A100");
+  ASSERT_STREQ(soundex,"A100");
 }
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_6) {
  //AAA
   char soundex[5];
   generateSoundex("OCjjnrr", soundex);
- // ASSERT_STREQ(soundex,"O256");
+  ASSERT_STREQ(soundex,"O256");
 }
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_7) {
  //AAA
   char soundex[5];
   generateSoundex("123123", soundex);
- // ASSERT_STREQ(soundex,"0000");
+ ASSERT_STREQ(soundex,"1000");
 }
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_8) {
  //AAA
   char soundex[5];
   generateSoundex("H3ll0W0rld", soundex);
-  ASSERT_STREQ(soundex,"H400");
+ ASSERT_STREQ(soundex,"H464");
 }
