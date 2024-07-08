fe397fb335a2ff59c4891f35f2cdf35ffb2942e0
navya7093 <navyakontala@gmail.com>
2024-07-08 22:15:56 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -37,3 +37,9 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_6) {
   generateSoundex("OCjjnrr", soundex);
  // ASSERT_STREQ(soundex,"O256");
 }
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_7) {
+ //AAA
+  char soundex[5];
+  generateSoundex("123123", soundex);
+ // ASSERT_STREQ(soundex,"0000");
+}
