8fa84bb08ffec820b37359d59705752b14cad25b
navya7093 <navyakontala@gmail.com>
2024-07-08 21:30:44 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -13,7 +13,7 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
   generateSoundex("CABBg2%", soundex);
   ASSERT_STREQ(soundex,"C120");
 }
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
  //AAA
   char soundex[5];
   generateSoundex("Emmb3$", soundex);
