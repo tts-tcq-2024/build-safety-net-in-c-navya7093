cca9aaf4f66948642c8766f8e52a0a7b737f251b
navya7093 <navyakontala@gmail.com>
2024-07-08 16:35:12 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,16 +4,19 @@
 #include <ctype.h>
 #include <string.h>
 
+// Function to get Soundex code for a given character
 char getSoundexCode(char c) {
     static const char *codes = "01230120022455012623010202";
     return isalpha(c) ? codes[toupper(c) - 'A'] : '0';
 }
 
+// Function to initialize the Soundex string with the first character
 void initializeSoundex(const char *name, char *soundex) {
     soundex[0] = toupper(name[0]);
     soundex[1] = '\0';
 }
 
+// Function to append the Soundex code to the Soundex string if valid
 void appendSoundexCode(char *soundex, int *sIndex, char code) {
     if (*sIndex < 4) {
         soundex[*sIndex] = code;
@@ -22,6 +25,7 @@ void appendSoundexCode(char *soundex, int *sIndex, char code) {
     }
 }
 
+// Function to fill the remaining positions of the Soundex string with '0'
 void fillSoundex(char *soundex, int sIndex) {
     while (sIndex < 4) {
         soundex[sIndex++] = '0';
@@ -29,6 +33,19 @@ void fillSoundex(char *soundex, int sIndex) {
     soundex[4] = '\0';
 }
 
+// Function to process the name and append appropriate Soundex codes
+void processName(const char *name, char *soundex, int *sIndex) {
+    char prevCode = getSoundexCode(name[0]);
+    for (int i = 1; name[i] != '\0' && *sIndex < 4; i++) {
+        char code = getSoundexCode(name[i]);
+        if (code != '0' && code != prevCode) {
+            appendSoundexCode(soundex, sIndex, code);
+            prevCode = code;
+        }
+    }
+}
+
+// Main function to generate the Soundex string
 void generateSoundex(const char *name, char *soundex) {
     if (!name || name[0] == '\0') {
         soundex[0] = '\0';
@@ -36,18 +53,8 @@ void generateSoundex(const char *name, char *soundex) {
     }
 
     initializeSoundex(name, soundex);
-
     int sIndex = 1;
-    char prevCode = getSoundexCode(name[0]);
-
-    for (int i = 1; name[i] != '\0' && sIndex < 4; i++) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            appendSoundexCode(soundex, &sIndex, code);
-            prevCode = code;
-        }
-    }
-
+    processName(name, soundex, &sIndex);
     fillSoundex(soundex, sIndex);
 }
 
